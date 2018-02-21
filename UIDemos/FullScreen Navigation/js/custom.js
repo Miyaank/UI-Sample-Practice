3b5180b496f73f7b593446bbd95284d45bbb5367
Mayank Lambhate <mayank.lambhate@hotwaxsystems.com>
2018-02-21 19:47:24 +0530

    Added : Font-awesome CSS files to project, added demo for fullscreen navigation.

---
+++ UIDemos/FullScreen Navigation/js/custom.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("fullscreen-nav-container").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("fullscreen-nav-container").style.width = "0%";
+}
