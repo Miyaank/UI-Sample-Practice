91ac671cd3faf79b230d165a03d53d10219937cb
Mayank Lambhate <mayank.lambhate@hotwaxsystems.com>
2018-02-24 13:52:13 +0530

    Moved UI demos to demos branch

--- UIDemos/FullScreen Navigation/js/custom.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-    document.getElementById("fullscreen-nav-container").style.width = "100%";
-}
-
-function closeNav() {
-    document.getElementById("fullscreen-nav-container").style.width = "0%";
-}
